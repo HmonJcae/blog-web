50f5947ea52e61296a69be8b428d7ec2698a474d
HmonJcae <1770592392@qq.com>
2020-05-05 12:43:16 +0800

    commit readme

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <Toolbar />
+    <v-content id="home">
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<script>
+import Toolbar from "./components/Toolbar";
+
+export default {
+  name: "App",
+
+  components: {
+    Toolbar
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="home">
+    <v-card max-width="900" class="mx-auto">
+      <v-spacer></v-spacer>
+
+      <v-list two-line subheader>
+        <v-list-item v-for="item in items" :key="item.title">
+          <v-list-item-avatar>
+            <v-icon :class="[item.iconClass]" v-text="item.icon"></v-icon>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon color="grey lighten-1">mdi-information</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+
+        <v-divider inset></v-divider>
+      </v-list>
+    </v-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
+  data: () => ({
+    items: [
+      {
+        icon: "folder",
+        iconClass: "grey lighten-1 white--text",
+        title: "Photos",
+        subtitle: "Jan 9, 2014"
+      },
+      {
+        icon: "folder",
+        iconClass: "grey lighten-1 white--text",
+        title: "Recipes",
+        subtitle: "Jan 17, 2014"
+      },
+      {
+        icon: "folder",
+        iconClass: "grey lighten-1 white--text",
+        title: "Work",
+        subtitle: "Jan 28, 2014"
+      }
+    ],
+    items2: [
+      {
+        icon: "assignment",
+        iconClass: "blue white--text",
+        title: "Vacation itinerary",
+        subtitle: "Jan 20, 2014"
+      },
+      {
+        icon: "call_to_action",
+        iconClass: "amber white--text",
+        title: "Kitchen remodel",
+        subtitle: "Jan 10, 2014"
+      }
+    ]
+  }),
   components: {
-    HelloWorld
+    //About: () => import('@/components/home/About')
   }
 };
 </script>
